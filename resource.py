c5aba57475350b099d0c3bbf2b40592ec4761963
Oliver <oliverng2512@icloud.com>
2022-05-06 18:13:25 +0100

    Show branch in version number

--- resource.py
+++ resource.py
@@ -24,10 +24,10 @@ import uuid
 
 from replace_forbidden import replace_forbidden
 
-__version__ = '0.0+ecadd31'
+__version__ = '0.0+7b6b0d1.main'
 __all__ = ['Resource', 'ConsumableResource', 'Location', 'Account']
 ABOUT = """resource module for 3org
-Version 0.0 (master@ecadd31)
+Version 0.0 (master@7b6b0d1.main)
 committed 2 May 2022
 Data version 0 (0x00000000)"""
 
